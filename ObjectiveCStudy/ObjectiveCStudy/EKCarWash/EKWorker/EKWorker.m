f65164e0f5831a902518c05009dec8cd02695e74
Egor <basterfly@gmail.com>
2017-06-09 16:23:34 +0300

    EKWorker.m

--- ObjectiveCStudy/ObjectiveCStudy/EKCarWash/EKWorker/EKWorker.m
+++ ObjectiveCStudy/ObjectiveCStudy/EKCarWash/EKWorker/EKWorker.m
@@ -23,7 +23,7 @@
     return self;
 }
 
-- (void)takeMoneyFromObject:(id<EKMoneyTransfer> )object {
+- (void)takeMoneyFromObject:(id<EKMoneyTransfer>)object {
     [self takeMoney:[object giveMoney]];
 }
 
